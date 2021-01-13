9b9a326482325cd659668dd840f67d024ae0705d
Jakob Persons <jakobep24@gmail.com>
2021-01-13 17:09:18 -0500

    added deliverables

--- index.js
+++ index.js
@@ -3,4 +3,60 @@ document.addEventListener("DOMContentLoaded", function() {
   init();
 
   // ADD CODE HERE!
+  document.addEventListener('keydown', function(event) {
+    const movesUl = document.querySelector('#moves-container')
+    // console.log(event.key)
+    if (event.key === 'ArrowDown') {
+      downLi = document.createElement('li')
+      downLi.id = 'downBtn'
+      downLi.innerText = 'down'
+      movesUl.append(downLi)
+    } else if (event.key === 'ArrowRight') {
+      rightLi = document.createElement('li')
+      rightLi.id = 'rightBtn'
+      rightLi.innerText = 'right'
+      movesUl.append(rightLi)
+    } else if (event.key === 'ArrowUp') {
+      upLi = document.createElement('li')
+      upLi.id = 'upBtn'
+      upLi.innerText = 'up'
+      movesUl.append(upLi)
+    } else if (event.key === 'ArrowLeft') {
+      leftLi = document.createElement('li')
+      leftLi.id = 'leftBtn'
+      leftLi.innerText = 'left'
+      movesUl.append(leftLi)
+    }
+  })
+  
+  const moveBtn = document.querySelector('#move-button')
+  const movesUl = document.querySelector('#moves-container')
+  
+  moveBtn.addEventListener('click', function() {
+    let firstLi = movesUl.querySelector('li')
+    move(firstLi.innerText)
+    firstLi.remove()
+  })
+
+
+
+  // const movesUl = document.querySelector('#moves-container')
+
+  movesUl.addEventListener('click', function(event) {
+    if (event.target.matches('#downBtn')) {
+      event.target.remove()
+    } else if (event.target.matches('#rightBtn')) {
+      event.target.remove()
+    } else if (event.target.matches('#upBtn')) {
+      event.target.remove()
+    } else if (event.target.matches('#leftBtn')) {
+      event.target.remove()
+    }
+  })
+
+
+
 });
+
+
+
